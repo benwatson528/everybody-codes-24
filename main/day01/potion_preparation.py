006a1fbd78c6d2707bb22fb262d4418d39907af1
Ben Watson <benwatson528@gmail.com>
2024-11-20 17:18:53 +0000

    Day 1 running as a single call

--- main/day01/potion_preparation.py
+++ main/day01/potion_preparation.py
@@ -1,17 +1,8 @@
 CREATURE_SCORES = {"A": 0, "B": 1, "C": 3, "D": 5, "x": 0}
+EXTRA_PORTIONS = {3: 6, 2: 2, 1: 0, 0: 0}
 
 
 def solve(creature_groups) -> int:
-    return sum(score_extra_potions(creature_group) +
+    return sum(EXTRA_PORTIONS[len([x for x in creature_group if x != "x"])] +
                sum(CREATURE_SCORES[creature] for creature in creature_group)
                for creature_group in creature_groups)
-
-
-def score_extra_potions(creature_group):
-    match len([x for x in creature_group if x != "x"]):
-        case 3:
-            return 6
-        case 2:
-            return 2
-        case _:
-            return 0
