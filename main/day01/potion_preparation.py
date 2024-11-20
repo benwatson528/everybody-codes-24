374d7e66da0acce5314f27d50a517c732f4d4263
Ben Watson <benwatson528@gmail.com>
2024-11-20 17:17:21 +0000

    Day 1 all parts

--- main/day01/potion_preparation.py
+++ main/day01/potion_preparation.py
@@ -1,11 +1,17 @@
-def solve(creatures) -> int:
-    return sum(sum_potions(creature) for creature in creatures)
+CREATURE_SCORES = {"A": 0, "B": 1, "C": 3, "D": 5, "x": 0}
 
 
-def sum_potions(creature):
-    match creature:
-        case "B":
-            return 1
-        case "C":
-            return 3
-    return 0
+def solve(creature_groups) -> int:
+    return sum(score_extra_potions(creature_group) +
+               sum(CREATURE_SCORES[creature] for creature in creature_group)
+               for creature_group in creature_groups)
+
+
+def score_extra_potions(creature_group):
+    match len([x for x in creature_group if x != "x"]):
+        case 3:
+            return 6
+        case 2:
+            return 2
+        case _:
+            return 0
--- tests/day01/test_potion_preparation.py
+++ tests/day01/test_potion_preparation.py
@@ -9,9 +9,33 @@ def test_p1_simple():
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 1299
+    assert solve(read_input("data/p1_input.txt")) == 1299
+
+
+def test_p2_simple():
+    assert solve(pair("AxBCDDCAxD")) == 28
+
+
+def test_p2_real():
+    assert solve(pair(read_input("data/p2_input.txt"))) == 5529
+
+
+def test_p3_simple():
+    assert solve(triple("xBxAAABCDxCC")) == 30
+
+
+def test_p3_real():
+    assert solve(triple(read_input("data/p3_input.txt"))) == 27734
+
+
+def pair(ls):
+    return zip(ls[::2], ls[1::2])
+
+
+def triple(ls):
+    return zip(ls[::3], ls[1::3], ls[2::3])
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
-        return f.read()
+        return f.read().strip()
